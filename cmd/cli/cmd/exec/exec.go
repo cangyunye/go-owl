1a12c335738497072001218a8e9765e509ba681f
cangyunye <cangyunye@163.com>
2026-08-03 22:49:51 +0800

    feat(i18n): add exec run command translation

--- cmd/cli/cmd/exec/exec.go
+++ cmd/cli/cmd/exec/exec.go
@@ -2,19 +2,16 @@ package exec
 
 import (
 	"github.com/spf13/cobra"
+
+	"github.com/cangyunye/go-owl/internal/i18n"
 )
 
 // NewExecCmd 创建执行命令
 func NewExecCmd() *cobra.Command {
 	execCmd := &cobra.Command{
 		Use:   "exec",
-		Short: "命令和脚本执行",
-		Long: `命令和脚本执行，支持以下操作：
-
-- run: 执行 Shell 命令
-- script: 执行脚本文件
-
-注意：剧本执行请使用 "owl playbook" 命令`,
+		Short: i18n.T("exec.cmd.short"),
+		Long:  i18n.T("exec.cmd.long"),
 	}
 
 	execCmd.AddCommand(NewRunCmd())
